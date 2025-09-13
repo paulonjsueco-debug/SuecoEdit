9dbe73cb3cefb3613651c6bfaeb9ab85be27e6c1
paulonjsueco-debug <paulo.njsueco@gmail.com>
2025-09-13 21:25:32 +0100

    Create service-worker.js
    esse é o service worker, o que ajuda o app a ser instalado nos dispoositivos

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'sueco-edit-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './favicon.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
+  'https://fonts.cdnfonts.com/s/72225/Valentine Baby.woff'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
